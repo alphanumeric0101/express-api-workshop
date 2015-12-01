74c2bba1aa6e4ff314538581aa551c97ec2cbd93
Enzo Matrix <alphanumeric@openmailbox.org>
2015-12-01 19:13:18 +0000

    initialized index.js and created dummy auth token

--- index.js
+++ index.js
@@ -3,12 +3,41 @@ var bodyParser = require('body-parser');
 
 var db = require('mysql');
 var connection = db.createConnection({
-    user: 'ziad_saab',
-    host: '127.0.0.1',
+    host: process.env.IP,
+    user: process.env.C9_USER,
+    password: '',
     database: 'addressbook'
 });
 
 var app = express();
 app.use(bodyParser.json());
 
-// app.get('...
+app.use(function(request, response, next){
+    request.accountId = 1;
+    next();
+});
+
+// app.get('/query', function(req, res) {
+//     connection.query("SELECT * from Account", function(err, result) {
+//         if (err) {
+//             console.log('error!');
+//         }
+//         else {
+//             console.log(result);
+//         }
+//     });
+// });
+
+// app.get('/query', function(req, res) {
+//     connection.query("SELECT * from Account", function(err, result) {
+       
+        
+//     });
+// });
+
+var server = app.listen(process.env.PORT, process.env.IP, function() {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
+});
