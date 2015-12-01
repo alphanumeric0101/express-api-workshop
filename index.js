fbef4982d83156ef4eaba27decf008bf38f03e92
Enzo Matrix <alphanumeric@openmailbox.org>
2015-12-01 20:57:20 +0000

    Added the Delete function for authorized accounts

--- index.js
+++ index.js
@@ -12,32 +12,76 @@ var connection = db.createConnection({
 var app = express();
 app.use(bodyParser.json());
 
-app.use(function(request, response, next){
+app.use(function(request, response, next) {
     request.accountId = 1;
     next();
 });
 
+///////////////////////////////////////////////////
+////////////////DISPLAY ENTRY//////////////////////
+/////////                               ///////////
+
 app.get('/AddressBooks/:id', function(req, res) {
     connection.query("SELECT * from AddressBook WHERE id=" + req.params.id, function(err, result) {
-        if (err) {
-            console.log('error!');
-        }
-        else if (result.accountId !== req.accountId) {
-            res.status(404).send("You don't have access to that account");
-        }
-        else {
-            console.log(result);
-            res.send(result);
-        }
+        result.forEach(function(ab) {
+            if (err) {
+                console.log('error!');
+            }
+            else if (ab.accountId !== req.accountId) {
+                res.status(404).send("You don't have access to that account");
+            }
+            else {
+                console.log(result);
+                res.send(result);
+            }
+        });
     });
 });
 
-// app.get('/query', function(req, res) {
-//     connection.query("SELECT * from Account", function(err, result) {
-       
-        
-//     });
-// });
+///////////////////////////////////////////////////
+////////////////// ADD ENTRY///////////////////////
+/////////                               ///////////
+
+app.post('/AddressBooks', function(req, res) {
+    if (req.body.name && req.accountId) {
+        connection.query("INSERT INTO AddressBook SET name='" + req.body.name + "', accountId=" + req.accountId, function(err, result) {
+            if (err) {
+                console.log("That's an error");
+            }
+            else {
+                connection.query("SELECT * FROM AddressBook WHERE id=" + result.insertId, function(err, displayResult) {
+                    if (err) {
+                        console.log("Error at the 2nd select level");
+                    }
+                    else {
+                        res.send(displayResult);
+                    }
+                });
+            }
+        });
+    }
+    else {
+        res.status(404).send('You have no books, please create an account');
+    }
+});
+
+///////////////////////////////////////////////////
+////////////////DELETE ENTRY///////////////////////
+/////////                               ///////////
+
+app.delete('/AddressBooks/:id', function(req, res) {
+    connection.query("DELETE from AddressBook WHERE AddressBook.id=" + req.params.id, function(err, result) {
+        if (err) {
+            console.log('bad query');
+        }
+        else if (result.accountId === req.accountID) {
+            res.send('AddressBook with id ' + req.params.id + ' has been deleted');
+        }
+        else {
+            res.status(403).send("You are not authorized to delete books on that account");
+        }
+    });
+});
 
 var server = app.listen(process.env.PORT, process.env.IP, function() {
     var host = server.address().address;
